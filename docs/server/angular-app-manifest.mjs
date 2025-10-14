fada48c3230c545dc0d81363e0bc650ea2022a56
Christia Tidwell <christiatidwell@Christias-MacBook-Air.local>
2025-10-14 11:18:03 -0500

    Deploy latest build

---
+++ docs/server/angular-app-manifest.mjs
@@ -0,0 +1,20 @@
+
+export default {
+  bootstrap: () => import('./main.server.mjs').then(m => m.default),
+  inlineCriticalCss: true,
+  baseHref: '/mySudoku/',
+  locale: undefined,
+  routes: [
+  {
+    "renderMode": 2,
+    "route": "/mySudoku"
+  }
+],
+  entryPointToBrowserMapping: undefined,
+  assets: {
+    'index.csr.html': {size: 442, hash: 'a6276b14da551e9b39725e0231f5323b17fc61fa6b39e8120e6a703cfb1d7f22', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 955, hash: 'f0f02dc7ee5f8ad40d3f79ad1b42d24b55f08ef487cf14d84ac68f37dc365ba5', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 26339, hash: '49411eddc7522ea7c1bccfbd4bb11d7beba9faa8f39450ed120cf85913110d0e', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
+  },
+};
